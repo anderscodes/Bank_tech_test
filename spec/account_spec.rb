b4138d79c7078de74872b9670c3210907b27696c
anderscodes <anders.j.persson@hotmail.com>
2018-06-20 15:14:29 +0100

    When a new account is created the balance is 0

---
+++ spec/account_spec.rb
@@ -0,0 +1,9 @@
+require './lib/account.rb'
+
+describe Account do
+  subject(:account) { described_class.new }
+
+  it ' A user creates a new account and the balance is zero' do
+    expect(account.balance).to eq(0)
+  end
+end
